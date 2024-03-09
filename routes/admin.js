d4046f37f2afa93ebf24dcdd0a3c2d5b42b46538
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-03-09 21:00:32 +0530

    routes are defined

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+//const adminMiddleware=require("../middleware/admin")
+const router=express.Router()
+
+
+router.post('/signup',(req,res)=>{
+
+})
+
+router.post('/courses',(res,req)=>{
+
+})
+
+router.get('/courses',(req,res)=>{
+
+})
+
+module.exports = router

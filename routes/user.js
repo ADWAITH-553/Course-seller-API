4d9251b480193828d9ff1c99cec2ee9d3f3dd7c1
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-03-17 12:07:19 +0530

    user registration

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const adminMiddleware=require("../middleware/admin")
 const router=express.Router()
+const {user,course}=require("../db/index")
 
-
-router.post('/signup',(req,res)=>{
-
+router.post('/signup',async (req,res)=>{
+    const username=req.body.username
+    const password=req.body.password
+    await user.create({
+        username,
+        password
+    })
+    res.json({message:"user created successfully"})
 })
+
 
 router.post('/courses',adminMiddleware,(res,req)=>{
 
